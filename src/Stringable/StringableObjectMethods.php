e6e7f94ebfcc4e0c864ba98e76a149899a71d574
Gabriel Tenita <the.ge@tenita.eu>
2024-12-10 17:34:57 +0200

    Fixed StringableObjectMethods::__toString() returning false

--- src/Stringable/StringableObjectMethods.php
+++ src/Stringable/StringableObjectMethods.php
@@ -19,6 +19,9 @@ trait StringableObjectMethods
 {
     public function __toString(): string
     {
-        return json_encode($this, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        return json_encode(
+            $this,
+            JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION
+        ) ?: 'FAILED_JSON_ENCODING';
     }
 }
