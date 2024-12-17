a4ef9059a50ac6da0d1d044c18990874977057b5
Gabriel Tenita <the.ge@tenita.eu>
2024-12-17 08:31:02 +0200

    Changed Stringable domain to String

---
+++ src/String/StringableObjectMethods.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Stringable object methods
+ * Classes using this trait have to implement \Stringable
+ *
+ * File name: StringableObjectMethods.php
+ * Created:   2024-12-10 16:44:44
+ * @author    Gabriel Tenita <dev2023@tenita.eu>
+ * @link      https://github.com/the-ge/
+ * @copyright Copyright (c) 2024-present Gabriel Tenita
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License version 2.0
+ */
+
+namespace TheGe\Shared\String;
+
+
+trait StringableObjectMethods
+{
+    public function __toString(): string
+    {
+        return json_encode(
+            $this,
+            JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION
+        ) ?: 'FAILED_JSON_ENCODING';
+    }
+}
